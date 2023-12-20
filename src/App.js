056856fd2c115fdce06d8d747a11d3449651e5f0
Oluwatobi Bamidele <teebams49@gmail.com>
2023-12-21 00:29:05 +0100

    feat: authorize when page mount

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
 import "./App.css";
 import * as sphinx from "sphinx-bridge-tobibams";
-import { useState } from "react";
+import { useState, useEffect, useCallback } from "react";
 
 function App() {
   const [enableText, setEnableText] = useState("");
-  async function good() {
+  // async function good() {
+  //   const result = await sphinx.enable();
+  //   console.log(result);
+  //   setEnableText(JSON.stringify(result));
+  // }
+
+  const good = useCallback(async () => {
     const result = await sphinx.enable();
     console.log(result);
     setEnableText(JSON.stringify(result));
-  }
+  }, []);
+
+  useEffect(() => {
+    setTimeout(async () => {
+      good();
+    }, 5000);
+  }, []);
 
   async function setBudget() {
     const result = await sphinx.setBudget();
